51fab2daf9c5f6c3767d5ffc2bc26b2f6fb92cf9
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2020-08-05 11:19:24 +0300

    Improve Latvian translations

--- src/main/java/lv/continuum/scorer/common/TranslationDictionary.java
+++ src/main/java/lv/continuum/scorer/common/TranslationDictionary.java
@@ -2,12 +2,14 @@ package lv.continuum.scorer.common;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 public class TranslationDictionary {
 
-    private static final String PROPERTIES_PATH = "properties/translation.properties";
-    private static final Properties properties = new Properties();
+    private static final String PROPERTIES_PATH = "translations/en.properties";
+    private static final Properties PROPERTIES = new Properties();
 
     private static TranslationDictionary instance;
 
@@ -15,7 +17,7 @@ public class TranslationDictionary {
         try {
             var file = getClass().getClassLoader().getResource(PROPERTIES_PATH).getFile();
             try (var fis = new FileInputStream(file)) {
-                properties.load(fis);
+                PROPERTIES.load(new InputStreamReader(fis, StandardCharsets.UTF_8));
             }
         } catch (IOException | NullPointerException e) {
             throw new IllegalStateException("Translation properties cannot be initialised", e);
@@ -35,9 +37,8 @@ public class TranslationDictionary {
         return instance;
     }
 
-    // TODO Add @NotNull to key
     public String getTranslation(String key) {
-        var value = properties.getProperty(key);
+        var value = PROPERTIES.getProperty(key);
         if (value == null) {
             value = key;
         }
