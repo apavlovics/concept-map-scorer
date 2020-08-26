815371a9c11ea0dab9103f3c29ea6f9f2ab8dd1a
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2020-08-26 09:53:58 +0300

    Complete ConceptMapParserTest

--- src/test/java/lv/continuum/scorer/logic/ConceptMapParserTest.java
+++ src/test/java/lv/continuum/scorer/logic/ConceptMapParserTest.java
@@ -3,29 +3,46 @@ package lv.continuum.scorer.logic;
 import lv.continuum.scorer.TestData;
 import lv.continuum.scorer.common.InvalidDataException;
 import org.junit.jupiter.api.Test;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ConceptMapParserTest {
 
     private final TestData data = TestData.getInstance();
     private final ConceptMapParser conceptMapParser = new ConceptMapParser();
 
-    private String makeFilePath(String fileName) {
+    private String makePath(String fileName) {
         return "src/test/resources/samples/" + fileName;
     }
 
     @Test
-    void parseStandard() throws ParserConfigurationException, InvalidDataException, SAXException, IOException {
-        assertEquals(data.conceptMapWithLevels, conceptMapParser.parse(makeFilePath("concept-map-with-levels.xml")));
+    void parseStandard() throws Exception {
+        assertEquals(
+                data.conceptMapWithLevels,
+                conceptMapParser.parse(makePath("concept-map-with-levels.xml"))
+        );
     }
 
     @Test
-    void parseIkas() throws ParserConfigurationException, InvalidDataException, SAXException, IOException {
-        assertEquals(data.conceptMapWithCycles, conceptMapParser.parse(makeFilePath("concept-map-with-cycles.xml")));
+    void parseIkas() throws Exception {
+        assertEquals(
+                data.conceptMapWithCycles,
+                conceptMapParser.parse(makePath("concept-map-with-cycles.xml"))
+        );
+    }
+
+    @Test
+    void parseInvalid() {
+        assertThrows(
+                IOException.class,
+                () -> conceptMapParser.parse(makePath("whatever.xml"))
+        );
+        assertThrows(
+                InvalidDataException.class,
+                () -> conceptMapParser.parse(makePath("concept-map-invalid.xml"))
+        );
     }
 }
