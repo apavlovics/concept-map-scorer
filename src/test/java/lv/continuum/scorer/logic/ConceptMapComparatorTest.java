05769c0554146001c3562c74d78263b1c8b830e2
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2020-08-26 16:27:18 +0300

    Extend ConceptMapComparatorTest

--- src/test/java/lv/continuum/scorer/logic/ConceptMapComparatorTest.java
+++ src/test/java/lv/continuum/scorer/logic/ConceptMapComparatorTest.java
@@ -5,34 +5,43 @@ import lv.continuum.scorer.common.InvalidDataException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class ConceptMapComparatorTest {
 
     private final TestData data = TestData.getInstance();
-    private final ConceptMapComparator conceptMapComparator = new ConceptMapComparator(
-            data.conceptMapSimilar1,
-            data.conceptMapSimilar2
-    );
 
     @Test
-    void compareUsingClosenessIndexes() {
-        assertEquals("0.786", conceptMapComparator.compareUsingClosenessIndexes());
-    }
+    void compareSimilar() throws InvalidDataException {
+        var comparator = new ConceptMapComparator(data.conceptMapSimilar1, data.conceptMapSimilar2);
+        assertEquals("0.786", comparator.compareUsingClosenessIndexes());
+        assertEquals("0.381", comparator.compareUsingImportanceIndexes());
+        assertEquals("0.667", comparator.compareUsingPropositionChains());
 
-    @Test
-    void compareUsingImportanceIndexes() throws InvalidDataException {
-        assertEquals("0.381", conceptMapComparator.compareUsingImportanceIndexes());
-    }
-
-    @Test
-    void compareUsingPropositionChains() throws InvalidDataException {
-        assertEquals("0.667", conceptMapComparator.compareUsingPropositionChains());
-    }
-
-    @Test
-    void compareUsingErrorAnalysis() throws InvalidDataException {
-        var similarityDegrees = conceptMapComparator.compareUsingErrorAnalysis();
+        var similarityDegrees = comparator.compareUsingErrorAnalysis();
         assertEquals("0.837", similarityDegrees.similarityDegree);
         assertEquals("0.404", similarityDegrees.weightedSimilarityDegree);
+    }
+
+    @Test
+    void compareNotSimilar() {
+        var comparator = new ConceptMapComparator(data.conceptMapWithLevels, data.conceptMapOther);
+        assertEquals("0.143", comparator.compareUsingClosenessIndexes());
+        assertThrows(InvalidDataException.class, comparator::compareUsingImportanceIndexes);
+        assertThrows(InvalidDataException.class, comparator::compareUsingPropositionChains);
+        assertThrows(InvalidDataException.class, comparator::compareUsingErrorAnalysis);
+    }
+
+    @Test
+    void compareEqual() throws InvalidDataException {
+        var comparator = new ConceptMapComparator(data.conceptMapWithLevels, data.conceptMapWithLevels);
+        assertEquals("1.000", comparator.compareUsingClosenessIndexes());
+        assertEquals("1.000", comparator.compareUsingImportanceIndexes());
+        assertEquals("1.000", comparator.compareUsingPropositionChains());
+
+        var similarityDegrees = comparator.compareUsingErrorAnalysis();
+        assertEquals("1.000", similarityDegrees.similarityDegree);
+        assertEquals("1.000", similarityDegrees.weightedSimilarityDegree);
     }
 
     private void assertEquals(String expected, double actual) {
